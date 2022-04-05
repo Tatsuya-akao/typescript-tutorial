596b2fdd3d59f3f4d0dd9d73d283329bd10d0eb0
tatsuya <a.tatsu0117@gmail.com>
2022-04-06 03:26:59 +0900

    16:50

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let name: any;
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+let printName: (name:string) => never;
+
+
+// type Person = {
+//   name: string;
+//   age?: number;
+// }
+
+// let person: Person = {
+//   name: 'Tatsuya',
+// }
+
+// let lotsOfPeople: Person[];
+
+let personName: unknown;
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
     </div>
   );
 }
